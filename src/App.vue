839574547c7d57188de1e13c5ea0b10a71440e35
zhuohui-xhh <1181754773@qq.com>
2024-06-02 15:04:42 +0800

    test：代码提交前进行eslin和代码格式化-有效

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-var a = 10
+var a = 103434
+var b = 456
+console.log(a, b)
 </script>
 
 <template>
