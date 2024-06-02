92942c12705dbc31c072abec54ca4ece25e351f2
zhuohui-xhh <1181754773@qq.com>
2024-06-02 19:58:57 +0800

    增加build打包配置，去除生产包的debuger和日志

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
+// npm i  terser   -D  剔除console.log
 // npm i vite-plugin-imagemin -D
 import viteImagemin from 'vite-plugin-imagemin' //图片压缩
 // npm i vite-plugin-compression -D
@@ -132,6 +133,56 @@ export default defineConfig(({ command, mode }) => {
           rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
+    },
+    //打包配置 默认这里是生产环境运行
+    build: {
+      //浏览器兼容性  "esnext"|"modules"
+      target: 'modules',
+      //指定输出路径
+      outDir: 'dist',
+      //生成静态资源的存放路径
+      assetsDir: 'assets',
+      //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
+      assetsInlineLimit: 4096,
+      //启用/禁用 CSS 代码拆分
+      cssCodeSplit: true,
+      //构建后是否生成 source map 文件
+      sourcemap: false,
+      //自定义底层的 Rollup 打包配置
+      rollupOptions: {
+        // 代码分割，配合组件的按需引入
+        output: {
+          entryFileNames: `assets/[name].[hash].js`,
+          chunkFileNames: `assets/[name].[hash].js`,
+          assetFileNames: `assets/[name].[hash].[ext]`
+        }
+      },
+      //@rollup/plugin-commonjs 插件的选项
+      commonjsOptions: {},
+      //构建的库
+      // lib: {},
+      //当设置为 true，构建后将会生成 manifest.json 文件
+      manifest: false,
+      // 设置为 false 可以禁用最小化混淆，
+      // 或是用来指定使用哪种混淆器
+      // boolean | 'terser' | 'esbuild'
+      minify: 'terser', //terser 构建后文件体积更小
+      //传递给 Terser 的更多 minify 选项。
+      terserOptions: {
+        //生产环境不要日志，去掉console,debugger
+        compress: {
+          drop_console: isProduction,
+          drop_debugger: isProduction
+        }
+      },
+      //设置为 false 来禁用将构建后的文件写入磁盘
+      write: true,
+      //默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录。
+      emptyOutDir: true,
+      //启用/禁用 brotli 压缩大小报告
+      // brotliSize: true,
+      //chunk 大小警告的限制
+      chunkSizeWarningLimit: 500
     }
   }
 })
